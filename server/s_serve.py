dd98930fe0a50f12b443cc807b8a7771836a57e8
Chunar <m18742021187@163.com>
2019-08-19 21:01:29 +0800

    optimization

--- server/s_serve.py
+++ server/s_serve.py
@@ -2,40 +2,61 @@ import socket
 import pymysql
 import json
 import struct
+import threading
 from into_db import Motor
 
-# define connection parameters
-host = '192.168.43.169'
-port = 9090
 
-# connect via socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a socket object, and declare the type
-server.bind((host, port))  # connect with ip and port
-server.listen(5)  # start to listen, and there can be 5 connections in queue
+class WSGIServer(object):
+    def __init__(self, host, port):
+        """init"""
+        # create a socket object, and declare the type
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# instance a Motor object
-mt = Motor()
+        # set host and port
+        self.server.bind((host, port))
 
-while True:
-    conn, addr = server.accept()  # conn is a new socket object, to deal with communication
-    print(conn, addr)
+        # start to listen, and there can be 30 connections at most in queue
+        self.server.listen(30)
+        # instance a Motor object
+        self.motor = Motor()
 
-    while True:
-        try:
-            length = struct.unpack('i', conn.recv(4))  # rceive the length of data
-            data = conn.recv(length[0])
-            print(type(data))  # show the type of the data received
+    def get_data(self, conn, addr):
+        """receive data and insert data into database"""
+        while True:
+            try:
+                length = struct.unpack('i', conn.recv(4))  # rceive the length of data
+                data = conn.recv(length[0])
 
-            # Translate byte data into dictionary
-            data_dict = json.loads(data.decode())
+                # Translate bytes data into dictionary
+                data_dict = json.loads(data.decode())
 
-            # insert data into database
-            mt.insert_data(data_dict)
+                # insert data into database
+                self.motor.insert_data(data_dict)
 
-            # check
-            print('recive:', data.decode())  # decode the data and print
-            conn.send(data.upper())  # send data back
-        except:
-            print('data error')
+                # check
+                print('recive:', data.decode())  # decode the data and print
+                conn.send(data.upper())  # send data back
+            except:
+                print('data error')
 
-    conn.close()
+    def run(self):
+        """get connected"""
+        while True:
+            conn, addr = self.server.accept()
+            print("connected with: {}".format(addr))
+
+            # when a new socket is connected, start a new thread
+            t1 = threading.Thread(target=self.get_data, args=(conn, addr))
+            t1.start()
+
+
+def main(host, port):
+    wsgi_server = WSGIServer(port)
+    wsgi_server.run()
+
+
+if __name__ == '__main__':
+    # set host and port
+    host, port = '172.17.52.39', 9876
+    main(host, port)
