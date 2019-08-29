efc5385bf91d2e05ec396b728edbf36005a6b2e3
Chunar <m18742021187@163.com>
2019-08-29 11:07:42 +0800

    add factory

---
+++ motor_f/into_db.py
@@ -0,0 +1,32 @@
+import pymysql
+from sql_helper import Helper
+
+
+class Motor():
+    def __init__(self):
+        # instance into_db object
+        self.db = Helper(host='',
+                         user='Chunar',
+                         password='chun.0927',
+                         database='motor_test',
+                         port=3306)
+        self.db.connect()
+
+    def insert_data(self, data_dict):
+        # insert data into database
+        try:
+            serial_number = data_dict.pop('serial_number')
+            create_time = data_dict.pop('create_time')
+
+            # insert data into every table
+            for sub_name in data_dict.keys():
+                table_name = serial_number + '_' + sub_name
+                value_list = data_dict[sub_name].split('/')
+
+                for value in value_list:
+                    sql = "INSERT INTO motor_test.{}(create_time, value) VALUES('{}', '{}')"\
+                        .format(table_name, create_time, value)
+                    self.db.insert(sql)
+
+        except:
+            print('no data')
---
+++ motor_f/motor_server.py
@@ -0,0 +1,79 @@
+    
+import socket
+import pymysql
+import json
+import struct
+import threading
+from into_db import Motor
+
+
+class WSGIServer(object):
+    def __init__(self, host, port):
+        """init"""
+        # create a socket object, and declare the type
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # set host and port
+        self.server.bind((host, port))
+
+        # start to listen, and there can be 30 connections at most in queue
+        self.server.listen(30)
+        # instance a Motor object
+        self.motor = Motor()
+        self.num = 0
+
+    def get_data(self, conn, addr):
+        """receive data and insert data into database"""
+        while True:
+            try:
+                length = struct.unpack('i', conn.recv(4))  # rceive the length of data
+                data_all = b''
+                l = length[0]
+
+                # get all of data
+                while l > 0: 
+                    data = conn.recv(l)
+                    data_all += data
+                    l -= len(data)
+                    print(l)
+
+                # Translate bytes data into dictionary
+                data_dict = json.loads(data_all.decode('utf-8'))
+
+                # insert data into database
+                self.motor.insert_data(data_dict)
+
+                # check
+                print('recive:', data.decode())  # decode the data and print
+                conn.send(data.upper())  # send data back
+            except:
+                if self.num < 20:
+                    self.num += 1
+                    print('data error')
+                else:
+                    print('connection over')
+                    self.num = 0
+                    conn.close()
+                    return
+
+    def run(self):
+        """get connected"""
+        while True:
+            conn, addr = self.server.accept()
+            print("connected with: {}".format(addr))
+
+            # when a new socket is connected, start a new thread
+            t1 = threading.Thread(target=self.get_data, args=(conn, addr))
+            t1.start()
+
+
+def main(host, port):
+    wsgi_server = WSGIServer(host, port)
+    wsgi_server.run()
+
+
+if __name__ == '__main__':
+    # set host and port
+    host, port = '192.168.43.169', 9876
+    main(host, port)
