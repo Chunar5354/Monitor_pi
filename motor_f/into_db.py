4b379c9abd8cf0e61ec477b2530f143dc017fdba
Chunar <m18742021187@163.com>
2019-09-25 14:59:29 +0800

    add thread

--- motor_f/into_db.py
+++ motor_f/into_db.py
@@ -1,16 +1,23 @@
 import pymysql
 from sql_helper import Helper
+import threading
 
 
 class Motor():
     def __init__(self):
         # instance into_db object
         self.db = Helper(host='',
-                         user='Chunar',
-                         password='chun.0927',
-                         database='monitoring_test',
+                         user='VentoAureo',
+                         password='GioGio114514',
+                         database='monitoring_Spike',
                          port=3306)
         self.db.connect()
+
+    def each_key(self, value_list, create_time, table_name):
+        for value in value_list:
+            sql = "INSERT INTO monitoring_Spike.{}(create_time, value) VALUES('{}', '{}')"\
+                        .format(table_name, create_time, value)
+            self.db.insert(sql)
 
     def insert_data(self, data_dict):
         # insert data into database
@@ -23,10 +30,13 @@ class Motor():
                 table_name = serial_number + '_' + sub_name
                 value_list = data_dict[sub_name].split('/')
 
-                for value in value_list:
-                    sql = "INSERT INTO monitoring_test.{}(create_time, value) VALUES('{}', '{}')"\
-                        .format(table_name, create_time, value)
-                    self.db.insert(sql)
+                t1 = threading.Thread(target=self.each_key, args=(value_list, create_time, table_name))
+                t1.start()
+
+                #for value in value_list:
+                #    sql = "INSERT INTO monitoring_Spike.{}(create_time, value) VALUES('{}', '{}')"\
+                #        .format(table_name, create_time, value)
+                #    self.db.insert(sql)
 
         except:
             print('no data')
